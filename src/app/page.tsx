b45af28d5337649c4e6c38e8145564fcdf5148dd
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-01-03 12:40:51 +0000

    feat: remove hr on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,13 @@ export default function Home() {
   return (
     <div className="grid min-h-screen items-center justify-center ">
       <main className="grid items-center justify-center max-w-xs">
-        <span className="text-4xl font-bold">
+        <span className="text-4xl font-bold mb-2">
           Alex <br />
           Bramhill
         </span>
-        <hr className="w-full mt-2 mb-4 bg-foreground h-1 animated-expand animated-delay-500ms" />
         <div className="mx-auto">
           <a href="https://www.linkedin.com/in/bramhill/">
-            <LinkedIn className="h-6 w-6 animated-fade-in animated-delay-1750ms" />
+            <LinkedIn className="h-6 w-6 animated-fade-in animated-delay-250ms" />
           </a>
         </div>
       </main>
