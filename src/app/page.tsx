43b0089ccf0c65bbe8ec48a1ace44d4e271a5a5f
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-01-03 13:06:28 +0000

    feat: update landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,23 @@ import { LinkedIn } from "./svg/linkedin";
 export default function Home() {
   return (
     <div className="grid min-h-screen items-center justify-center ">
-      <main className="grid items-center justify-center max-w-xs">
-        <span className="text-4xl font-bold mb-2">
+      <main className="grid gap-3 items-center justify-center max-w-xs">
+        <span className="text-4xl font-bold">
           Alex <br />
           Bramhill
         </span>
-        <div className="grid grid-cols-[auto_auto] gap-3 mx-auto animated-fade-in animated-delay-250ms">
-          <a href="https://www.linkedin.com/in/bramhill/">
+        <div className="grid grid-cols-[auto-auto] grid-rows-[auto-auto] gap-3 mx-fill animate-fade-in">
+          <hr className="col-span-2 w-full bg-foreground h-1" />
+          <a
+            href="https://www.linkedin.com/in/bramhill/"
+            className="justify-self-end"
+          >
             <LinkedIn className="h-7 w-7" />
           </a>
-          <a href="https://github.com/alexbramhill/">
+          <a
+            href="https://github.com/alexbramhill/"
+            className="justify-self-start"
+          >
             <Github className="h-7 w-7" />
           </a>
         </div>
