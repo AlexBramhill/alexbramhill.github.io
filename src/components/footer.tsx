0bd1aa9ae3c499cc3a725028a263ed349ef321e4
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-01-06 22:41:24 +0000

    feat: remove unneeded code

--- src/components/footer.tsx
+++
@@ -1,6 +0,0 @@
-export const Footer = () => (
-  <footer className="p-2 text-center">
-    <hr className="w-sm bg-subtle max-w-xs mx-auto" />
-    <span className="text-subtle text-xs p-2"></span>
-  </footer>
-);
