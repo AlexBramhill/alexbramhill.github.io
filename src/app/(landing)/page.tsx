b6c083b2cefbcb756ea8e2fc0793197df6c1990f
Alex <47955140+AlexBramhill@users.noreply.github.com>
2025-01-12 02:16:11 +0000

    feat: remove animations of front page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,50 +1,46 @@
+import React from "react";
 import { TextLink } from "@/components/text-link";
 import { Github } from "../svg/github";
 import { LinkedIn } from "../svg/linkedin";
 
+const HeaderInfo = () => (
+  <div className="grid grid-cols-[1fr,auto] gap-3 items-end">
+    <h1 className="text-4xl font-bold">
+      Alex <br />
+      Bramhill
+    </h1>
+    <div className="grid gap-y-6 gap-x-3 mb-2 grid-cols-[auto,auto]">
+      <a href="https://github.com/alexbramhill/">
+        <Github className="h-7 w-7" />
+      </a>
+      <a href="https://www.linkedin.com/in/bramhill/">
+        <LinkedIn className="h-7 w-7" />
+      </a>
+    </div>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <p>
+      Backend-focused full-stack senior software developer currently working at{" "}
+      <TextLink href={"https://www.softwire.com"}>Softwire</TextLink>.
+    </p>
+    <p>
+      Previously studied and practiced architecture at world-leading
+      universities and firms, specialising in machine learning and computational
+      complex geometry.
+    </p>
+  </div>
+);
+
 export default function Home() {
   return (
     <div className="grid place-items-center min-h-dvh px-5 sm:px-10">
-      <div className="grid gap-3 max-w-sm">
-        <div className="grid grid-cols-[1fr,auto] gap-3 items-end">
-          <h1 className="text-4xl font-bold">
-            Alex <br />
-            Bramhill
-          </h1>
-          <div className="grid gap-3 mb-2 grid-cols-[auto,auto] animate-fade-in opacity-0">
-            <a href="https://github.com/alexbramhill/">
-              <Github className="h-7 w-7" />
-            </a>
-            <a href="https://www.linkedin.com/in/bramhill/">
-              <LinkedIn className="h-7 w-7" />
-            </a>
-          </div>
-        </div>
-        <hr className="w-full bg-foreground h-1 animate-stretch-width opacity-0" />
-
-        <div className="text-left animate-fade-in opacity-0">
-          <p className="pt-0">
-            Backend-focused full-stack senior software developer currently
-            working at{" "}
-            <TextLink href={"https://www.softwire.com"}>Softwire</TextLink>.
-          </p>
-          <p>
-            Previously studied and practiced architecture at world-leading
-            universities and firms, specialising in machine learning and
-            computational complex geometry.
-          </p>
-        </div>
+      <div className="grid gap-y-6 max-w-sm">
+        <HeaderInfo />
+        <About />
       </div>
     </div>
   );
 }
-
-Home.getLayout = function getLayout(page: React.ReactNode) {
-  return (
-    <>
-      OVERRIDE
-      {page}
-      tester
-    </>
-  );
-};
