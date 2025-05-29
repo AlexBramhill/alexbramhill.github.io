b6776ab0efefbe7687e9fd7a389349d6a4280a69
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-05-29 01:59:29 +0100

    feat: update blog header

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -4,20 +4,21 @@ import { LinkedIn } from "@/app/svg/linkedin";
 
 export const Header = () => (
   <header className="w-full max-w-4xl pb-24 mx-auto mt-4 flex  items-center justify-between">
-    <h1 className="text-xl font-bold pb-1">
-      <NavLink
-        href={"/"}
-        options={{
-          classNameOverrides:
-            "font-bold after:hover:opacity-0 border-2 p-1 hover:border-spot rounded-full",
-        }}
-      >
-        ab
-      </NavLink>
-    </h1>
-    <nav className="flex space-x-4 items-center">
+    <div className="flex pb-1 space-x-4 items-center">
+      <h1 className="text-xl font-bold">
+        <NavLink
+          href={"/"}
+          options={{
+            classNameOverrides:
+              "font-bold after:hover:opacity-0 border-2 p-1 hover:border-spot rounded-full",
+          }}
+        >
+          ab
+        </NavLink>
+      </h1>
       <NavLink href={"/blog"}>Blog</NavLink>
-      <NavLink href={"/about"}>About</NavLink>
+    </div>
+    <nav className="flex space-x-4 items-center">
       <div>
         <a href="https://github.com/alexbramhill/">
           <Github className="h-7 w-7" />
