387fff543548fe856fc7644900a5dcb1ac4ed463
Alex Bramhill <alexbramhill@gmail.com>
2026-07-12 21:46:27 +0100

    feat: port to vite

---
+++ src/components/text-link.tsx
@@ -0,0 +1,29 @@
+export type TextLinkProps = {
+    href: URL;
+    children: React.ReactNode;
+};
+
+export const TextLink = ({
+                             href,
+                             children,
+                         }: Readonly<TextLinkProps>) => {
+
+    const classNames = `
+        relative
+        text-spot
+        after:absolute
+        after:left-0
+        after:-bottom-0.5
+        after:ease-in-out
+        after:duration-300
+        after:bg-spot
+        after:h-0.5
+        after:opacity-0
+        after:w-full
+        hover:after:opacity-100
+    `;
+
+    return <a href={href.toString()} className={classNames}>
+        {children}
+    </a>
+};
