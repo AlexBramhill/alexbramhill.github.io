058b4013ee80f1e8dec8744be817b69f90c5ee87
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-09-08 21:37:59 +0100

    feat: add eslint-config-prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import {defineConfig} from "eslint/config";
-import {reactRefresh} from "eslint-plugin-react-refresh";
-import reactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig } from "eslint/config";
+import { reactRefresh } from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-        plugins: {js},
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite(),
-        ],
-        languageOptions: {globals: globals.browser}
-    },
-    tseslint.configs.recommended]);
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite(),
+      eslintConfigPrettier,
+    ],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
