5882708d62d31286d65c4e4e6ad57859085571d0
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-09-08 21:34:33 +0100

    chore: update eslint to ts file, bump vers

---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import {defineConfig} from "eslint/config";
+import {reactRefresh} from "eslint-plugin-react-refresh";
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        plugins: {js},
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite(),
+        ],
+        languageOptions: {globals: globals.browser}
+    },
+    tseslint.configs.recommended]);
