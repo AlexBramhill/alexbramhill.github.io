b41ae7a6b8ab7eb2045399f82c4f2c0d4ae35aad
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-09-08 21:13:45 +0100

    feat: initial setup of tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import * as path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import {tanstackRouter} from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
     plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
         react(),
         babel({presets: [reactCompilerPreset()]}),
         tailwindcss(),
